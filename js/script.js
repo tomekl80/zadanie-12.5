b5b56938a05b205138b1714dd132ba5e15df1427
Tomasz Lis <tomasz.lis@o2.pl>
2018-09-16 00:42:39 +0200

    Add basic code

---
+++ js/script.js
@@ -0,0 +1,6 @@
+'use scrict';
+
+// Links ceriables
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
